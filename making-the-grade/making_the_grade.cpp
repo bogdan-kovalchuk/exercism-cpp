1d3ebe0224b7c2e2412cffbbcb81ebeacaf3e309
bogdan-kovalchuk <bogdankovalchuk@gmail.com>
2026-04-02 12:58:41 +0300

    Complete making-the-grade solution

--- making-the-grade/making_the_grade.cpp
+++ making-the-grade/making_the_grade.cpp
@@ -24,14 +24,25 @@ int count_failed_students(std::vector<int> student_scores)
         if (score <= 40)
             count++;
     }
-    return 0;
+    return count;
 }
 
 // Create a list of grade thresholds based on the provided highest grade.
 std::array<int, 4> letter_grades(int highest_score)
 {
-    // TODO: Implement letter_grades
-    return {};
+    const int FAIL_THRESHOLD = 40;
+    const int FIRST_PASS_SCORE = FAIL_THRESHOLD + 1;
+
+    int interval_size = (highest_score - FAIL_THRESHOLD) / 4;
+
+    std::array<int, 4> thresholds{};
+
+    for (int i = 0; i < 4; ++i)
+    {
+        thresholds[i] = FIRST_PASS_SCORE + i * interval_size;
+    }
+
+    return thresholds;
 }
 
 // Organize the student's rank, name, and grade information in ascending order.
@@ -39,7 +50,18 @@ std::vector<std::string> student_ranking(
     std::vector<int> student_scores, std::vector<std::string> student_names)
 {
     // TODO: Implement student_ranking
-    return {};
+    std::vector<std::string> ranking;
+    ranking.reserve(student_scores.size());
+
+    for (size_t i = 0; i < student_scores.size(); ++i)
+    {
+        ranking.push_back(
+            std::to_string(i + 1) + ". " +
+            student_names[i] + ": " +
+            std::to_string(student_scores[i]));
+    }
+
+    return ranking;
 }
 
 // Create a string that contains the name of the first student to make a perfect
@@ -48,5 +70,12 @@ std::string perfect_score(std::vector<int> student_scores,
                           std::vector<std::string> student_names)
 {
     // TODO: Implement perfect_score
+    for (size_t i = 0; i < student_scores.size(); ++i)
+    {
+        const int MAX_SCORE = 100;
+
+        if (student_scores[i] == MAX_SCORE)
+            return student_names[i];
+    }
     return "";
 }
